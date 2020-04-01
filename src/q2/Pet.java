41bc5637c3a9719cfd50dfa6f43b6490415f05fe
Jake Mcneill <mcneillj@cardiff.ac.uk>
2020-04-01 18:53:48 +0100

    Started work on question 2

---
+++ src/q2/Pet.java
@@ -0,0 +1,14 @@
+/*
+ *	Module: CM1210 CW1
+ *	Question: 2
+ *	Name: Jake Mcneill
+ *	Student Number: c1931370
+ */
+package q2;
+
+public class Pet {
+
+	protected String name;
+	protected int age;
+
+}
