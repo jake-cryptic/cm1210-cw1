7971b462484b601a3ccbe5a79c8bc9214a2534e1
Jake Mcneill <mcneillj@cardiff.ac.uk>
2020-04-01 15:05:05 +0100

    More progress on 1b Asks for user input, checks it in a basic way Attempts to swap... not tested yet

--- src/q1b/Magic.java
+++ src/q1b/Magic.java
@@ -10,7 +10,7 @@ import java.util.Random;
 
 public class Magic extends Square {
 
-	public Magic(int usize){
+	public Magic(int usize) {
 		super(usize);
 
 		int max 	= (int) Math.pow(usize, 2),
@@ -33,7 +33,28 @@ public class Magic extends Square {
 		}
 	}
 
-	public void shuffle(){
+	public void swap(int x, int y, String dir) {
+		int newX = x,
+			newY = y;
+
+		// Adjust coordinates depending on direction
+		switch (dir){
+			case "U": newY++; break;
+			case "D": newY--; break;
+			case "L": newX--; break;
+			case "R": newX++; break;
+		}
+
+		// Get data from old positions
+		int tmpOld = getPos(x, y);
+		int tmpNew = getPos(newX, newY);
+
+		// Set data for new positions
+		setPos(newX, newY, tmpOld);
+		setPos(x, y, tmpNew);
+	}
+
+	public void shuffle() {
 		Random rand = new Random();
 		for (int i = data.length - 1; i > 0; i--) {
 			for (int j = data[i].length - 1; j > 0; j--) {
--- src/q1b/Main.java
+++ src/q1b/Main.java
@@ -6,32 +6,59 @@
  */
 package q1b;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
+	private static String[] validMoves = new String[]{"U", "D", "L", "R"};
+
 	public static void main(String[] args) {
-		int userSquareSize = getSquareSize();
+		Input in = new Input();
+		int userSquareSize = getSquareSize(in);
+
+		List<String> validMovesList = Arrays.asList(validMoves);
 
 		Magic square = new Magic(userSquareSize);
 
 		String originalSquare = square.toString();
 		System.out.println("Here's the unshuffled square: \n" + originalSquare);
 
+		// Shuffle square
 		square.shuffle();
 
 		String shuffledSquare = square.toString();
 		System.out.println("Here's the shuffled square: \n" + shuffledSquare);
 
-		// Shuffle square
-
-
 		// While user square string != originalSquare, keep playing
 		int numMoves = 0;
+		while (!square.toString().equals(originalSquare)){
+			String move = in.getInput("Enter a move");
+			String[] components = move.split(" ");
 
+			System.out.println(components.length);
+
+			if (components.length != 3) {
+				System.out.println("Please enter an input that has the format: x y direction");
+				continue;
+			}
+
+			int x = Integer.parseInt(components[0]);
+			int y = Integer.parseInt(components[1]);
+			String dir = components[2].toUpperCase();
+
+			if (!validMovesList.contains(dir)) {
+				System.out.println("Valid directions include: U, D, L, R");
+				continue;
+			}
+
+			square.swap(x, y, dir);
+
+			numMoves++;
+		}
 	}
 
-	private static int getSquareSize(){
-		Input in = new Input();
-
+	private static int getSquareSize(Input in){
 		int squareSize;
 		boolean isAllowed;
 
